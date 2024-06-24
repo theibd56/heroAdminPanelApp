517f96f77a6cbdb41e46d82b2f2b351ac51b5c39
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2024-06-24 13:38:14 +0700

    update filter

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -8,7 +8,7 @@ import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 
 const HeroesList = () => {
-    const {heroes, heroesLoadingStatus} = useSelector(state => state);
+    const {filteredHeroes, heroesLoadingStatus} = useSelector(state => state);
     const dispatch = useDispatch();
     const {request} = useHttp();
 
@@ -57,7 +57,7 @@ const HeroesList = () => {
         })
     }
 
-    const elements = renderHeroesList(heroes);
+    const elements = renderHeroesList(filteredHeroes);
     return (
         <TransitionGroup component="ul">
             {elements}
